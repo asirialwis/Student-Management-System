1db9da42d97143662487da6b65eb9c0294a81c7a
asirialwis <asirichathurika900@gmail.com>
2023-12-25 21:11:52 +0530

    post route added

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -13,17 +13,16 @@ app.use(bodyParser.json());
 
 const URL = process.env.MONGODB_URL;
 
-// mongoose.connect(URL, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
-// });
 mongoose.connect(URL);
-
 
 const connection = mongoose.connection;
 connection.once("open",()=>{
     console.log("Mongodb connection success!");
 })
+
+const studentRouter = require('./routes/Students.js')
+
+app.use("/student",studentRouter);
 
 app.listen(PORT,()=>{
     console.log(`Server is up and running on port number: ${PORT}`);
